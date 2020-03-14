57df46839f67aecce43f617d0155667d38e10aea
John Shepard <shepard.jh@gmail.com>
2020-03-13 22:49:03 -0400

    Switched from Miles to Km to KM to Miles

--- section1-6.py
+++ section1-6.py
@@ -1,9 +1,9 @@
 # Exercise 1-6: KM to Miles Converter
 # Create a program that asks the user for a number in kilometers and converts it to miles.
-print("Welcome to the Miles to Kilometers Converter.")
+print("Welcome to the Kilometers to Miles Converter.")
 # Ask the user for a number in kilometers.
-miles = float(input("How many miles would you like to convert to kilometers? "))
+kilometers = float(input("How many kilomerters would you like to convert to miles? "))
 # Convert the input to miles using: 1 mile = 1.606344 Km
-kilometers = miles * 1.606344
+miles = kilometers / 1.606344
 # Print the result.
-print(str(miles) + " miles converts to " + str(kilometers) + " kilometers.")
+print(str(kilometers) + " kilometers converts to " + str(miles) + " miles.")

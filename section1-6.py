19991cd57f84ec5f83a228afbe9b14bbd2f391db
John Shepard <shepard.jh@gmail.com>
2020-03-13 23:01:31 -0400

    Added rounding

--- section1-6.py
+++ section1-6.py
@@ -4,6 +4,6 @@ print("Welcome to the Kilometers to Miles Converter.")
 # Ask the user for a number in kilometers.
 kilometers = float(input("How many kilomerters would you like to convert to miles? "))
 # Convert the input to miles using: 1 mile = 1.606344 Km
-miles = kilometers / 1.606344
+miles = round( kilometers / 1.606344, 3)
 # Print the result.
 print(str(kilometers) + " kilometers converts to " + str(miles) + " miles.")
